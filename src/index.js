ff81143bf68de4e063f27db4c11ec7ad64502878
Raaj <raaj.ahab@gmail.com>
2025-01-21 13:06:11 +0000

    fixes import paths

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 // Export components from base
-export { default as Button } from './base/Button';
-export { default as Container } from './base/Container';
-export { default as Image } from './base/Image';
-export { default as Text } from './base/Text';
+export { default as Button } from './components/base/Button';
+export { default as Container } from './components/base/Container';
+export { default as Image } from './components/base/Image';
+export { default as Text } from './components/base/Text';
 
 // Export components from composite
-export { default as Card } from './composite/Card';
+export { default as Card } from './components/composite/Card';
 
 // Export components from editor
-export { default as Canvas } from './editor/Canvas';
-export { default as ElementWrapper } from './editor/ElementWrapper';
-export { default as Toolbox } from './editor/Toolbox';
-export { default as Topbar } from './editor/Topbar';
+export { default as Canvas } from './components/editor/Canvas';
+export { default as ElementWrapper } from './components/editor/ElementWrapper';
+export { default as Toolbox } from './components/editor/Toolbox';
+export { default as Topbar } from './components/editor/Topbar';
 
 // Export components from user
-export { default as UserButton } from './user/Button';
-export { default as UserCard } from './user/Card';
-export { default as UserContainer } from './user/Container';
-export { default as UserDelete } from './user/Delete';
-export { default as UserImage } from './user/Image';
-export { default as UserText } from './user/Text';
-export { default as UserTopbar } from './user/Topbar';
-export { default as UserToolbox } from './user/Toolbox';
-export { default as ErrorBoundary } from './user/ErrorBoundary';
+export { default as UserButton } from './components/user/Button';
+export { default as UserCard } from './components/user/Card';
+export { default as UserContainer } from './components/user/Container';
+export { default as UserDelete } from './components/user/Delete';
+export { default as UserImage } from './components/user/Image';
+export { default as UserText } from './components/user/Text';
+export { default as UserTopbar } from './components/user/Topbar';
+export { default as UserToolbox } from './components/user/Toolbox';
+export { default as ErrorBoundary } from './components/user/ErrorBoundary';
