fb68632d23913c1c66b6d27b06d4809eecdaac93
Raaj <raaj.ahab@gmail.com>
2025-01-22 12:17:24 +0000

    changes build system

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser'; // Default export
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
 import json from '@rollup/plugin-json';
+import path from 'path'; // For resolving paths
+import cryptoBrowserify from 'crypto-browserify';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -26,6 +27,7 @@ export default {
     peerDepsExternal(),
     resolve({
       extensions: ['.js', '.jsx'],
+      preferBuiltins: false, // Prevents Node.js builtins from being bundled
     }),
     commonjs(),
     json(),
@@ -34,7 +36,17 @@ export default {
       babelHelpers: 'runtime',
       presets: ['@babel/preset-react'],
     }),
-    terser(), // Use the default export here
+    terser(), // Minify the output
   ],
   external: ['react', 'react-dom'],
+  resolve: {
+    alias: {
+      // Polyfill `node:crypto` to `crypto-browserify`
+      'node:crypto': 'crypto-browserify',
+    },
+    fallback: {
+      // Provide the fallback for `crypto`
+      crypto: path.resolve('node_modules/crypto-browserify'),
+    },
+  },
 };
